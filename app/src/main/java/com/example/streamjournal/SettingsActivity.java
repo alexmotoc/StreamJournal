5d5bcc26321c0d785d20b4c11f4da09196bc9739
Alex Motoc <alexmotoc97@gmail.com>
2020-05-04 19:33:40 +0100

    Refactored the settings activity.

--- app/src/main/java/com/example/streamjournal/SettingsActivity.java
+++ app/src/main/java/com/example/streamjournal/SettingsActivity.java
@@ -35,6 +35,11 @@ public class SettingsActivity extends AppCompatActivity {
 
         // Display available effects fetched from the Rest Api
         final AsyncHttpClient client = new AsyncHttpClient();
+
+        getEffects(client, context, settings);
+    }
+
+    public void getEffects(final AsyncHttpClient client, final Context context, final LinearLayout settings) {
         client.get("https://tungsten.alexlogan.co.uk/effect/b5583fa0-60ac-4ce1-8ba5-352d80757933/", new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, final JSONObject response) {
@@ -49,41 +54,23 @@ public class SettingsActivity extends AppCompatActivity {
                         RelativeLayout option = new RelativeLayout(context);
                         RelativeLayout.LayoutParams optionParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
                                 RelativeLayout.LayoutParams.WRAP_CONTENT);
-                        optionParams.setMargins(0, Math.round(10 * getResources().getDisplayMetrics().density), 0 ,0);
+                        optionParams.setMargins(0, pxToDp(10), 0 ,0);
                         option.setLayoutParams(optionParams);
 
                         RelativeLayout.LayoutParams sectionHeaderParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
                                 RelativeLayout.LayoutParams.WRAP_CONTENT);
 
                         if (i == 0) {
-                            TextView header = new TextView(context);
-                            sectionHeaderParams.setMargins(0, Math.round(50 * getResources().getDisplayMetrics().density), 0 ,0);
-                            header.setLayoutParams(sectionHeaderParams);
-                            header.setText(getString(R.string.basic));
-                            header.setTextSize(24);
-                            header.setTypeface(ResourcesCompat.getFont(context, R.font.raleway_medium));
+                            TextView header = createTextView(context, new int[] {}, new int[] {0, 50, 0, 0}, getString(R.string.basic), 24, -1);
                             settings.addView(header);
                         } else if (i == 2) {
-                            TextView header = new TextView(context);
-                            sectionHeaderParams.setMargins(0, Math.round(30 * getResources().getDisplayMetrics().density), 0 ,0);
-                            header.setLayoutParams(sectionHeaderParams);
-                            header.setText(getString(R.string.advanced));
-                            header.setTextSize(24);
-                            header.setTypeface(ResourcesCompat.getFont(context, R.font.raleway_medium));
+                            TextView header = createTextView(context, new int[] {}, new int[] {0, 30, 0, 0}, getString(R.string.basic), 24, -1);
                             settings.addView(header);
                         }
 
                         // Create option description
-                        TextView optionName = new TextView(context);
-                        RelativeLayout.LayoutParams optionNameParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
-                                RelativeLayout.LayoutParams.WRAP_CONTENT);
-                        optionNameParams.addRule(RelativeLayout.ALIGN_PARENT_START);
-                        optionNameParams.addRule(RelativeLayout.CENTER_IN_PARENT);
-                        optionName.setLayoutParams(optionNameParams);
-                        optionName.setText(effect.getString("name"));
-                        optionName.setTextSize(16);
-                        optionName.setTextColor(getResources().getColor(R.color.black));
-                        optionName.setTypeface(ResourcesCompat.getFont(context, R.font.raleway_medium));
+                        TextView optionName = createTextView(context, new int[] {RelativeLayout.ALIGN_PARENT_START, RelativeLayout.CENTER_IN_PARENT},
+                                new int[] {0, 0, 0, 0}, effect.getString("name"), 16, getResources().getColor(R.color.black));
 
                         // Create option toggle switch
                         Switch optionStatus = new Switch(context);
@@ -99,114 +86,20 @@ public class SettingsActivity extends AppCompatActivity {
                                 try {
                                     effect.put("isActive", isChecked);
 
-                                    JSONObject updatedEffects = new JSONObject();
-                                    updatedEffects.put("templates", templates);
-                                    updatedEffects.put("effects", effects);
-                                    RequestParams params = new RequestParams();
-                                    params.put("effects", updatedEffects.toString());
-                                    client.put("https://tungsten.alexlogan.co.uk/effect/b5583fa0-60ac-4ce1-8ba5-352d80757933/", params, new JsonHttpResponseHandler());
+                                    updateEffects(client, templates, effects);
                                 } catch (JSONException e) {
                                     e.printStackTrace();
                                 }
                             }
 
                         });
+
                         option.addView(optionName);
                         option.addView(optionStatus);
                         settings.addView(option);
                     }
 
-                    client.get("https://tungsten.alexlogan.co.uk/overlays/", new JsonHttpResponseHandler() {
-                        @Override
-                        public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                            // Add section header
-                            RelativeLayout.LayoutParams templateHeaderParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
-                                    RelativeLayout.LayoutParams.WRAP_CONTENT);
-
-                            TextView header = new TextView(context);
-                            templateHeaderParams.setMargins(0, Math.round(30 * getResources().getDisplayMetrics().density), 0 ,0);
-                            header.setLayoutParams(templateHeaderParams);
-                            header.setText(getString(R.string.templates));
-                            header.setTextSize(24);
-                            header.setTypeface(ResourcesCompat.getFont(context, R.font.raleway_medium));
-                            settings.addView(header);
-
-                            // Add options to enable templates
-                            for (int i=0; i < response.length(); i++) {
-                                try {
-                                    final JSONObject templateInfo = response.getJSONObject(i);
-
-                                    // Create template container
-                                    RelativeLayout template = new RelativeLayout(context);
-                                    RelativeLayout.LayoutParams optionParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
-                                            RelativeLayout.LayoutParams.WRAP_CONTENT);
-                                    optionParams.setMargins(0, Math.round(10 * getResources().getDisplayMetrics().density), 0 ,0);
-                                    template.setLayoutParams(optionParams);
-
-                                    // Create template checkbox
-                                    CheckBox templateStatus = new CheckBox(context);
-                                    RelativeLayout.LayoutParams templateStatusParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
-                                            RelativeLayout.LayoutParams.WRAP_CONTENT);
-                                    templateStatusParams.addRule(RelativeLayout.ALIGN_PARENT_START);
-                                    String[] url = templateInfo.getString("url").split("/");
-                                    String id = url[url.length- 1];
-                                    if (templates.toString().contains(id)) {
-                                        templateStatus.setChecked(true);
-                                    }
-                                    templateStatus.setLayoutParams(templateStatusParams);
-                                    templateStatus.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
-                                        @Override
-                                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                                            try {
-                                                String[] url = templateInfo.getString("url").split("/");
-                                                String id = url[url.length- 1];
-
-                                                if (isChecked) {
-                                                    templates.put(id);
-                                                } else {
-                                                    for (int i=0; i < templates.length(); i++) {
-                                                        if (templates.get(i).equals(id)) {
-                                                            templates.remove(i);
-                                                        }
-                                                    }
-                                                }
-
-                                                JSONObject updatedEffects = new JSONObject();
-                                                updatedEffects.put("templates", templates);
-                                                updatedEffects.put("effects", effects);
-                                                RequestParams params = new RequestParams();
-                                                params.put("effects", updatedEffects.toString());
-                                                client.put("https://tungsten.alexlogan.co.uk/effect/b5583fa0-60ac-4ce1-8ba5-352d80757933/", params, new JsonHttpResponseHandler());
-                                            } catch (JSONException e) {
-                                                e.printStackTrace();
-                                            }
-                                        }
-
-                                    });
-
-                                    // Create template description
-                                    TextView templateName = new TextView(context);
-                                    RelativeLayout.LayoutParams templateNameParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
-                                            RelativeLayout.LayoutParams.WRAP_CONTENT);
-                                    templateNameParams.setMargins(Math.round(50 * getResources().getDisplayMetrics().density), Math.round(10 * getResources().getDisplayMetrics().density), 0 ,0);
-                                    templateNameParams.addRule(RelativeLayout.CENTER_VERTICAL);
-                                    templateName.setLayoutParams(templateNameParams);
-                                    templateName.setText(templateInfo.getString("name"));
-                                    templateName.setTextSize(16);
-                                    templateName.setTextColor(getResources().getColor(R.color.black));
-                                    templateName.setTypeface(ResourcesCompat.getFont(context, R.font.raleway_medium));
-
-                                    template.addView(templateStatus);
-                                    template.addView(templateName);
-                                    settings.addView(template);
-
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
-                    });
+                    getTemplates(client, context, settings, templates, effects);
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -215,5 +108,140 @@ public class SettingsActivity extends AppCompatActivity {
         });
     }
 
+    public void getTemplates(final AsyncHttpClient client, final Context context, final LinearLayout settings,
+                             final JSONArray templates, final JSONArray effects) {
+        client.get("https://tungsten.alexlogan.co.uk/overlays/", new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                // Add section header
+                TextView header = createTextView(context, new int[] {}, new int[] {0, 30, 0, 0}, getString(R.string.templates), 24, -1);
+                settings.addView(header);
 
+                // Add options to enable templates
+                for (int i=0; i < response.length(); i++) {
+                    try {
+                        final JSONObject templateInfo = response.getJSONObject(i);
+
+                        // Create template container
+                        RelativeLayout template = new RelativeLayout(context);
+                        RelativeLayout.LayoutParams optionParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
+                                RelativeLayout.LayoutParams.WRAP_CONTENT);
+                        optionParams.setMargins(0, pxToDp(10), 0 ,0);
+                        template.setLayoutParams(optionParams);
+
+                        // Create template checkbox
+                        CheckBox templateStatus = new CheckBox(context);
+                        RelativeLayout.LayoutParams templateStatusParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+                                RelativeLayout.LayoutParams.WRAP_CONTENT);
+                        templateStatusParams.addRule(RelativeLayout.ALIGN_PARENT_START);
+
+                        // Parse url to obtain template id
+                        String[] url = templateInfo.getString("url").split("/");
+                        String id = url[url.length- 1];
+                        if (templates.toString().contains(id)) {
+                            templateStatus.setChecked(true);
+                        }
+
+                        templateStatus.setLayoutParams(templateStatusParams);
+                        templateStatus.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+                            @Override
+                            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                                try {
+                                    String[] url = templateInfo.getString("url").split("/");
+                                    String id = url[url.length- 1];
+
+                                    if (isChecked) {
+                                        templates.put(id);
+                                    } else {
+                                        // Remove template from active list if it is unchecked
+                                        for (int i=0; i < templates.length(); i++) {
+                                            if (templates.get(i).equals(id)) {
+                                                templates.remove(i);
+                                            }
+                                        }
+                                    }
+
+                                    updateEffects(client, templates, effects);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                        });
+
+                        // Create template description
+                        TextView templateName = createTextView(context, new int[] {RelativeLayout.CENTER_VERTICAL}, new int[] {50, 10, 0, 0},
+                                templateInfo.getString("name"), 16, getResources().getColor(R.color.black));
+
+                        template.addView(templateStatus);
+                        template.addView(templateName);
+                        settings.addView(template);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     *
+     * @param px Number of pixels
+     * @return Device pixels (dp) corresponding to a pixel value
+     */
+    public int pxToDp(int px) {
+        return Math.round(px * getResources().getDisplayMetrics().density);
+    }
+
+    /**
+     *
+     * @param context Activity context
+     * @param rules Array of rules (integers) to be applied to the layout parameters
+     * @param margins Array of margins (size 4) ordered as follows: left, top, right, bottom
+     * @param text The text to be rendered in the view
+     * @param size The size of the text
+     * @param color The color of the text
+     * @return A new TextView element with the specified properties
+     */
+    public TextView createTextView(Context context, int[] rules, int[] margins, String text, int size, int color) {
+        TextView textView = new TextView(context);
+
+        RelativeLayout.LayoutParams textViewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        textViewParams.setMargins(pxToDp(margins[0]), pxToDp(margins[1]), pxToDp(margins[2]) ,pxToDp(margins[3]));
+
+        for (int rule: rules) {
+            textViewParams.addRule(rule);
+        }
+
+        textView.setLayoutParams(textViewParams);
+        textView.setText(text);
+        textView.setTextSize(size);
+
+        if (color != -1) {
+            textView.setTextColor(color);
+        }
+
+        textView.setTypeface(ResourcesCompat.getFont(context, R.font.raleway_medium));
+
+        return textView;
+    }
+
+    /**
+     *
+     * @param client Client used to perform PUT request
+     * @param templates Array of template ids
+     * @param effects Array of effect objects containing the effect's name and status
+     * @throws JSONException Throws exception if JSON put operation fails
+     */
+    public void updateEffects(AsyncHttpClient client, JSONArray templates, JSONArray effects) throws JSONException {
+        JSONObject updatedEffects = new JSONObject();
+        updatedEffects.put("templates", templates);
+        updatedEffects.put("effects", effects);
+        RequestParams params = new RequestParams();
+        params.put("effects", updatedEffects.toString());
+        client.put("https://tungsten.alexlogan.co.uk/effect/b5583fa0-60ac-4ce1-8ba5-352d80757933/", params, new JsonHttpResponseHandler());
+    }
 }
